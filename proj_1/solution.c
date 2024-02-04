185b5dd4702194e27cf005c33c5f661fa8f1211a
Joe Burchett <5685541+Trogluddite@users.noreply.github.com>
2024-02-03 20:18:59 -0800

    add starter code for project 1

---
+++ proj_1/solution.c
@@ -0,0 +1,25 @@
+/* This one is for you to finish!  Have fun! */
+#include "tester.h"
+#include <stdio.h>
+
+int find_best(void*, int, size_t){
+  return 0;
+}
+
+int* sorted_copy(int*, size_t){
+  int someVal = 0;
+  return &someVal;
+}
+
+int combine(int, int){
+  return 0;
+}
+int main() {
+  struct bundle{
+    
+  }
+  int result = find_best(&main, 3, sizeof(size_t));
+  printf("result: %d", result);
+
+	return 0;
+}
