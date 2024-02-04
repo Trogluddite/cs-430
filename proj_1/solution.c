26dadaebab3230f79395dff1bacc91408ef915c1
Joe Burchett <5685541+Trogluddite@users.noreply.github.com>
2024-02-04 12:23:36 -0800

    sort passing  tests, 1/3 there!

--- proj_1/solution.c
+++ proj_1/solution.c
@@ -1,25 +1,48 @@
 /* This one is for you to finish!  Have fun! */
 #include "tester.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-int find_best(void*, int, size_t){
-  return 0;
-}
 
-int* sorted_copy(int*, size_t){
-  int someVal = 0;
-  return &someVal;
+int* sorted_copy(int* source_arr, size_t alen){
+  //copy the source_arr
+  int* sorted = malloc( sizeof(source_arr[0]) * alen);
+  for(int i=0; i < alen; i++){
+    sorted[i] = source_arr[i];
+  }
+  // I am advanced prograemar
+  // I am bubble sort them baubles
+  int sortlen = alen-1;
+  while(sortlen > 0){
+    int slice_idx = 0;
+    while(slice_idx < sortlen){
+      if(sorted[slice_idx] > sorted[slice_idx +1]){
+        int t = sorted[slice_idx];
+        sorted[slice_idx] = sorted[slice_idx + 1];
+        sorted[slice_idx + 1] = t;
+      }slice_idx++;
+    }sortlen--;
+  }
+  return sorted;
 }
 
 int combine(int, int){
   return 0;
 }
-int main() {
-  struct bundle{
-    
-  }
-  int result = find_best(&main, 3, sizeof(size_t));
-  printf("result: %d", result);
+int find_best(void*, int, size_t){
+  return 0;
+}
 
-	return 0;
+int main() {
+  struct bundle_struct bunst;
+  bunst.sorted_copy = sorted_copy;
+  bunst.combine = combine;
+  bunst.find_best = find_best;
+
+  int sortme[6] = {34,43,51,67,91,100};
+  size_t arrlen = sizeof(sortme) / sizeof(sortme[0]);
+  //int* sorted = bs.sorted_copy(sortme, arrlen);
+  test(&bunst);
+	
+  return 0;
 }
