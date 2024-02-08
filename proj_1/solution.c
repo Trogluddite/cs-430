6539b7615a4afb5b780ea50633f0f7ffdd6ea58b
Joe Burchett <5685541+Trogluddite@users.noreply.github.com>
2024-02-07 20:52:08 -0800

    save minor cleanup

--- proj_1/solution.c
+++ proj_1/solution.c
@@ -1,8 +1,24 @@
 /* This one is for you to finish!  Have fun! */
 #include "tester.h"
-#include <stdio.h>
 #include <stdlib.h>
 
+int find_best(void* fp_list, int f_param, size_t num_fps){
+  int best_idx = 0;
+  int best_res = 0;
+  int curr_idx = 0;
+  // de-referencing & casting fp_list -- (**cfunc) is a pointer-to-pointer; returns int; takes int param
+  // iteration comparison: cfunc's address is < (fp_list's address + number of iteratiions * size of first value in cfunc
+  // cfunc++ increases address value by word-size? sizeof(*cfunc), I think?
+  for (int (**cfunc)(int) = fp_list; cfunc < fp_list + (num_fps * sizeof(*cfunc)); cfunc++){
+    int tf_res = (*cfunc)(f_param);
+    if(tf_res > best_res){
+      best_res = tf_res;
+      best_idx = curr_idx;
+    }
+    curr_idx++;
+  }
+  return best_idx;
+}
 
 int* sorted_copy(int* source_arr, size_t alen){
   //copy the source_arr
@@ -34,25 +50,6 @@ int combine(int a, int b){
   int shift_res = (or_res >> 3);
   int end_byte_zeros_mask = (~0) << 8;
   return shift_res & end_byte_zeros_mask;
-}
-
-int find_best(void* fp_list, int f_param, size_t num_fps){
-  int best_idx = 0;
-  int best_res = 0;
-
-  int curr_idx = 0;
-  // de-referencing & casting fp_list -- (**cfunc) is a pointer-to-pointer; returns int; takes int param
-  // iteration comparison: cfunc's address is < (fp_list's address + number of iteratiions * size of first value in cfunc
-  // cfunc++ increases address value by word-size? sizeof(*cfunc), I think?
-  for (int (**cfunc)(int) = fp_list; cfunc < fp_list + (num_fps * sizeof(*cfunc)); cfunc++){
-    int tf_res = (*cfunc)(f_param);
-    if(tf_res > best_res){
-      best_res = tf_res;
-      best_idx = curr_idx;
-    }
-    curr_idx++;
-  }
-  return best_idx;
 }
 
 int main() {
