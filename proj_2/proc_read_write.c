771c69d0fe6ca3ff70a33fb40ab4af174f4d1a0c
Joe Burchett <5685541+Trogluddite@users.noreply.github.com>
2024-03-10 12:53:54 -0700

    add input validation to proc read

--- proj_2/proc_read_write.c
+++ proj_2/proc_read_write.c
@@ -9,11 +9,12 @@ static char procfs_buffer[PROCFS_MAX_SIZE];
 static unsigned long procfs_buffer_size = 0;
 
 char text[] = "There was an armadillo which had a lot of money to invest.  Gold, stocks, property, and bitcoin were all too high!";
+char inbuff[PROCFS_MAX_SIZE] = "L0";
 
 ssize_t read_simple(struct file *filp, char *buf, size_t count, loff_t *offp ) 
 {
 	char* readspot = text + *offp;
-	size_t readlen = strlen(readspot) > count? count:strlen(readspot);
+	size_t readlen = strlen(readspot) > count ? count : strlen(readspot);
 	printk("proc_read just ran!  count = %lu, sizeof(loff_t) = %lu, *offp = %llu\n", count, sizeof(loff_t), *offp);	
 	if(*offp >= strlen(text))
 			return 0;
@@ -31,10 +32,27 @@ ssize_t write_simple(struct file *file, const char *buffer,
   }
 
   if (copy_from_user(procfs_buffer, buffer, procfs_buffer_size) ){
+    printk("copy_from_user failed with input buffer of size %lu", procfs_buffer_size);
     return -EFAULT;
   }
-  printk("proc_write just ran! count = %lu, sizeof(loff_t) = %lu, *ppos= %llu\n", count, sizeof(loff_t), *ppos);
-  strncpy(text, buffer, count);
+  strncpy(inbuff, buffer, count);
+
+  if(count > 3){
+    printk("Invalid control sequence, expected input of no more than 2 characters but got %s", inbuff);
+    return -EINVAL;
+  }
+  else if( inbuff[0] != 'L' && inbuff[0] != 'D'){
+    printk("Invalid control sequence, expected first character of input to be L or D but got %s", inbuff);
+    return -EINVAL;
+  }
+  else if( ((char)inbuff[1] - '0') > 7 || ((char)inbuff[1] - '0') < 0){
+    printk(
+      "Invalid control sequence, second character of input to be a number in the range 0-7 but input was %s",
+      inbuff
+    );
+    return -EINVAL;
+
+  }
   return procfs_buffer_size;
 }
 
